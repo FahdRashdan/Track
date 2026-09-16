544e33f672766977156cf0f99a74e63907773360
Fahd Rashdan <fahdrashdan@gmail.com>
2026-09-16 11:51:49 -0700

    Backend ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,12 @@ import { initDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
+import job from "./config/cron.js";
+
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 //Middleware
 app.use(rateLimiter); // Apply rate limiter middleware to all routes
@@ -12,6 +16,10 @@ app.use(rateLimiter); // Apply rate limiter middleware to all routes
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
 
 
 
